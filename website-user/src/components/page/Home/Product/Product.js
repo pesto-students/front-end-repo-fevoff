c4acf4a24ad37cdf0ef15006734907160d9acd47
MahipalSinghBarva <mahipalsingh450@gmail.com>
2024-03-19 00:52:42 +0530

    CATEGORY API AT HOME

--- website-user/src/components/page/Home/Product/Product.js
+++ website-user/src/components/page/Home/Product/Product.js
@@ -5,26 +5,26 @@ import { getProduct, clearErrors } from "../../../../Action/productAction";
 import Loader from "../../../Layout/Loader";
 import ProductCard from "../../../ProductCard/ProductCard";
 
-const Product = () => {
-  const relatedCategories = [
-    { id: 1, name: "Wedding" },
-    { id: 2, name: "For Mens" },
-    { id: 3, name: "For Womens" },
-    { id: 4, name: "Party Wear" },
-    { id: 5, name: "Night Wear" },
-  ];
+import { getCategory } from "../../../../Action/productAction";
 
+import { useAlert } from "react-alert";
+
+const Product = () => {
+  
+
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products } = useSelector((state) => state.products);
+  const { category: categoryData } = useSelector((state) => state.category);
 
-
-  const limitProduct = products.slice(0, 5)
+  const limitProduct = products.slice(0, 5);
 
   useEffect(() => {
     if (error) {
       alert.error(error);
       dispatch(clearErrors());
     }
+    dispatch(getCategory());
     dispatch(getProduct());
   }, [dispatch, error]);
 
@@ -37,9 +37,14 @@ const Product = () => {
           <div className="grid md:pb-6 md:pt-12 py-8">
             <div className="box-heading text-center mb-4">
               <h2 className="italic text-3xl">Popular Products</h2>
-              <p className="md:px-40 px-4 py-4">In publishing and graphic design, Lorem ipsum is a placeholder
-                text commonly. <label className="hidden md:block">Used to demonstrate the visual form of a document or
-                  a typeface without relying.</label></p>
+              <p className="md:px-40 px-4 py-4">
+                In publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly.{" "}
+                <label className="hidden md:block">
+                  Used to demonstrate the visual form of a document or a
+                  typeface without relying.
+                </label>
+              </p>
             </div>
 
             <div className="grid md:grid-cols-5 grid-cols-2 md:gap-5 gap-2 product-listing">
@@ -53,27 +58,25 @@ const Product = () => {
             <div className="box-heading text-center mb-4">
               <h2 className="italic text-3xl">Related Products</h2>
               <div className="md:flex justify-center my-4 hidden">
-                {
-                  relatedCategories.map((relatedCategory, index) => {
-                    return (
-                      <h4 className="px-8 italic">{relatedCategory.name}</h4>
-                    )
-                  })
-                }
+              {categoryData &&
+              categoryData.data.data &&
+              categoryData.data.data.map((relatedCategory, index) => {
+                  return (
+                    <h4 className="px-8 italic">{relatedCategory.name}</h4>
+                  );
+                })}
               </div>
             </div>
 
             <div className="grid md:grid-cols-5 grid-cols-2 md:gap-5 gap-2 product-listing">
-              {
-                limitProduct.map((product, index) => (
-                  <ProductCard product={product} key={index} />
-                ))
-              }
-              {
-                limitProduct.map((product, index) => (
-                  <ProductCard product={product} key={index} />
-                ))
-              }
+              {limitProduct.map((product, index) => (
+                <ProductCard product={product} key={index} />
+              ))}
+
+              {limitProduct.map((product, index) => (
+                
+                <ProductCard product={product} key={index} />
+              ))}
             </div>
           </div>
         </>
