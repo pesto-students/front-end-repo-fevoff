c47b5b196fa3b81bfc4866578b891677c9eeafab
rjshah1902 <rjshah1902@gmail.com>
2024-03-19 00:12:49 +0530

    cart page done

--- website-user/src/components/page/Cart/Cart.js
+++ website-user/src/components/page/Cart/Cart.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Loader, Trash } from "lucide-react";
+import { Loader, Minus, Plus, Trash } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-
+import CommonBanner from "../../CommonBanner/CommonBanner";
+import Breadcrumbs from './../../Breadcrumbs/Breadcrumbs';
 import {
   updateCart,
   removeItemsToCart,
   getCartItems,
 } from "../../../Action/cartAction";
 import { clearErrors } from "../../../Action/cartAction";
-// import { ADD_TO_CART } from "../../../Constants/cartConstants";
 import { useAlert } from "react-alert";
+import "./cart.css";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -22,7 +23,7 @@ const Cart = () => {
   const { cartItems, error, loading } = useSelector((state) => state.cart);
 
   // console.log(cartItems);
-  
+
 
   const incressQuantity = (productId, quantity, availableQty) => {
     const newQty = quantity + 1;
@@ -44,12 +45,12 @@ const Cart = () => {
 
   const deleteCartItems = (productId) => {
     dispatch(removeItemsToCart(userId, productId))
-    .then(() => {
-      alert.success("Item deleted to cart successfully!");
-    })
-    .catch((error) => {
-      alert.error(`Failed to delete item to cart: ${error.message}`);
-    });
+      .then(() => {
+        alert.success("Item deleted to cart successfully!");
+      })
+      .catch((error) => {
+        alert.error(`Failed to delete item to cart: ${error.message}`);
+      });
   };
 
   const handleQtyChange = (e, productId) => {
@@ -90,201 +91,129 @@ const Cart = () => {
       {loading ? (
         <Loader />
       ) : (
-        <div className="bg-gradient-to-t from-yellow-100 via-pink-100 to-yellow-100 italic font-semibold">
-          <div className="mx-auto max-w-7xl px-2 lg:px-0">
-            <div className="mx-auto max-w-2xl py-8 lg:max-w-7xl">
-              <h6 className="text-2xl font-bold tracking-tight text-black sm:text-2xl">
-                Home > Cart
-              </h6>
-              <form className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
-                <section
-                  aria-labelledby="cart-heading"
-                  className="rounded-l border border-gray-500  lg:col-span-8"
-                >
-                  <h2 id="cart-heading" className="sr-only">
-                    Items in your shopping cart
-                  </h2>
-                  <ul role="list" className="divide-y divide-gray-200">
-                    {cartItems &&
-                      cartItems.data && cartItems.data.items && 
-                      cartItems.data.items.map((product, productIdx) => (
-                        <div key={product._id} className="">
-                          <li className="flex py-6 sm:py-6">
-                            <div className="flex-shrink-0">
-                              <img
-                                src={product.productMainImage}
-                                alt={product.name}
-                                className="sm:h-38 sm:w-38 h-24 w-24 rounded-md object-contain object-center"
-                              />
-                            </div>
+        <>
+          <CommonBanner pageTitle={"Cart Details"} />
+          <Breadcrumbs breadcumr1="Cart Details" />
+          <div className="container mx-auto my-5">
+            <form className="lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
+              <section aria-labelledby="cart-heading" className="cart-listing-border lg:col-span-8 rounded-md ">
+                <h2 id="cart-heading" className="sr-only">
+                  Items in your shopping cart
+                </h2>
+                <div role="list" className="divide-y divide-gray-200 cart-items">
+                  {
+                    cartItems &&
+                    cartItems.data && cartItems.data.items &&
+                    cartItems.data.items.map((product, productIdx) => (
+                      <div key={product._id} className="grid grid-cols-5">
+                        <div className="col-span-1 img-section">
+                          <img
+                            src={product.productMainImage}
+                            alt={product.name}
+                            className="product-cart-image"
+                          />
+                        </div>
 
-                            <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                              <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                                <div>
-                                  <div className="flex justify-between">
-                                    <h3 className="text-sm">
-                                      <a
-                                        href={product.href}
-                                        className="font-semibold text-black"
-                                      >
-                                        Product: {product.name}
-                                      </a>
-                                    </h3>
-                                  </div>
-                                  <div className="mt-1 flex flex-col text-sm">
-                                    {product.size ? (
-                                      <p className="border-l border-gray-200 text-sm text-gray-500">
-                                        Size: {product.size}
-                                      </p>
-                                    ) : null}
-                                    <p className="text-sm text-gray-500">
-                                      Brand: {product.brand}
-                                    </p>{" "}
-                                    <p className="text-sm text-gray-500">
-                                      Brand: {product.category}
-                                    </p>
-                                  </div>
-                                  <div className="mt-1 flex items-end">
-                                    <p className="text-xs font-medium text-gray-500 line-through">
-                                      Price: {product.productMrp}
-                                    </p>{" "}
-                                    <p className="text-xs font-medium text-gray-500 ">
-                                      Price: {product.productPrice}
-                                    </p>
-                                    <p className="text-sm font-medium text-gray-900">
-                                      &nbsp;&nbsp;{product.price}
-                                    </p>
-                                    &nbsp;&nbsp;
-                                    {/* <p className="text-sm font-medium text-green-500">
-                                      {product.sizeVariation}
-                                    </p> */}
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </li>
-                          <div className="mb-2 flex flex-col sm:flex-row">
-                            <div className="min-w-24 flex">
-                              <button
-                                type="button"
-                                className="h-10 w-10 border btn"
-                                onClick={() =>
-                                  decreseQuantity(
-                                    product.productId,
-                                    product.quantity
-                                  )
-                                }
-                              >
-                                -
-                              </button>
-                              <input
-                                type="text"
-                                className="mx-1 h-12 w-12 rounded-md border text-center "
-                                readOnly
-                                value={product.quantity}
-                                onChange={(e) =>
-                                  handleQtyChange(
-                                    e,
-                                    product.productId,
-                                    product.quantity
-                                  )
-                                }
-                              />
-                              <button
-                                type="button"
-                                className="flex h-10 w-10 items-center justify-center btn"
-                                onClick={() =>
-                                  incressQuantity(
-                                    product.productId,
-                                    product.quantity,
-                                    product.availableQty
-                                  )
-                                }
-                              >
-                                +
-                              </button>
-                            </div>
-                            <div className="ml-6 flex text-sm">
-                              <button
-                                type="button"
-                                className="flex items-center space-x-1 px-2 py-1 pl-0 "
-                              >
-                                <Trash
-                                  onClick={() =>
-                                    deleteCartItems(product.productId)
-                                  }
-                                  size={10}
-                                  className="text-red-500 border border-red-500 w-12 h-5 btn"
-                                />
-                              </button>
-                            </div>
+                        <div className="col-span-3">
+                          <div className="cart-product-details py-4">
+                            <label className="text-xl font-bold">
+                              {product.name}
+                            </label>
+                            <p className="text-xl">
+                              {product.size ? (
+                                "Size: " + product.size
+                              ) : " Free Size"}
+                            </p>
+                            <p className="text-xl font-medium ">
+                              Price: <del><sub>&#8377;{product.productMrp}</sub></del> &#8377;{product.productPrice}
+                            </p>
+                            <p className="mt-2">Vendor: Fevoff PVT. LTD</p>
                           </div>
                         </div>
-                      ))}
-                  </ul>
-                </section>
-                {/* Order summary */}
-                <section
-                  aria-labelledby="summary-heading"
-                  className="mt-16 rounded-md border border-black lg:col-span-4 lg:mt-0 lg:p-0"
-                >
-                  <h2
-                    id="summary-heading"
-                    className="border-b border-gray-200 px-4 py-3 text-lg font-medium text-gray-900 sm:p-4"
-                  >
-                    Price Details
-                  </h2>
-
-                  <div>
-                    <dl className=" space-y-1 px-2 py-4">
-                      <div className="flex items-center justify-between">
-                        <dt className="text-sm text-gray-800">Price:</dt>
-                        <dd className="text-sm font-medium text-gray-900">
-                          ₹ {price}
-                        </dd>
+                        <div className="col-span-1 qty-delete-section p-2">
+                          <button type="button" className="btn-delete" onClick={() => deleteCartItems(product.productId)} >
+                            <Trash size={20} />
+                          </button>
+                          <div className="flex justify-between">
+                            <button type="button" className="qty btn-minus" onClick={() =>
+                              decreseQuantity(product.productId, product.quantity)
+                            }>
+                              <Minus />
+                            </button>
+                            <input type="text"
+                              className="text-center w-full bg-transparent border border-black font-bold text-lg qty"
+                              readOnly
+                              value={product.quantity}
+                              onChange={(e) =>
+                                handleQtyChange(
+                                  e,
+                                  product.productId,
+                                  product.quantity
+                                )
+                              }
+                            />
+                            <button type="button" className="qty btn-add" onClick={() =>
+                              incressQuantity(product.productId, product.quantity, product.availableQty)
+                            } >
+                              <Plus />
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                      <div className="flex items-center justify-between pt-4">
-                        <dt className="flex items-center text-sm text-gray-800">
-                          <span>Discount:</span>
-                        </dt>
-                        <dd className="text-sm font-medium text-green-700">
-                          ₹ {discount}
-                        </dd>
-                      </div>
-                      <div className="flex items-center justify-between py-4">
-                        <dt className="flex text-sm text-gray-800">
-                          <span>Delivery Charges</span>
-                        </dt>
-                        <dd className="text-sm font-medium text-green-700">
-                          Free
-                        </dd>
-                      </div>
-                      <div className="flex items-center justify-between border-y border-dashed py-4 ">
-                        <dt className="text-base font-medium text-gray-900">
-                          Total Amount:
-                        </dt>
-                        <dd className="text-base font-medium text-gray-900">
-                          ₹ {totalAmount}
-                        </dd>
-                      </div>
-                    </dl>
-                    <div className="px-2 pb-4 font-medium text-green-700">
-                      You will save ₹ {discount} on this order
-                    </div>
-                  </div>
-                </section>
-                <button
-                  onClick={checkoutHandlear}
-                  type="button"
-                  className="flex btn mt-5 lg:ml-0 lg:col-span-12 w-full sm:w-96 bg-transparent border-red-500 hover:bg-yellow-500"
-                >
+                    ))
+                  }
+                </div>
+                <button onClick={checkoutHandlear} type="button" className="btn-checkout">
                   Process To Checkout
                 </button>
-              </form>
-            </div>
+              </section>
+
+              <section aria-labelledby="summary-heading" className="rounded-md cart-listing-border lg:col-span-4 lg:mt-0 lg:p-0">
+                <h2 className="border-b px-4 py-3 text-2xl font-bold sm:p-4">
+                  Price Details
+                </h2>
+
+                <div className=" space-y-1 px-4 py-4">
+                  <div className="flex items-center justify-between">
+                    <dt className="text-lg font-semibold text-dark">Price:</dt>
+                    <dd className="text-sm font-bold text-gray-900">
+                      ₹ {price}
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between pt-4">
+                    <dt className="flex items-center text-lg font-semibold text-dark">
+                      <span>Discount:</span>
+                    </dt>
+                    <dd className="text-sm font-bold text-green-700">
+                      ₹ {discount}
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between py-4">
+                    <dt className="flex text-lg font-semibold text-dark">
+                      <span>Delivery Charges</span>
+                    </dt>
+                    <dd className="text-sm font-bold text-green-700">
+                      Free
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between border-y border-dashed py-4 ">
+                    <dt className="text-base font-bold text-gray-900">
+                      Total Amount:
+                    </dt>
+                    <dd className="text-base font-bold text-gray-900">
+                      ₹ {totalAmount}
+                    </dd>
+                  </div>
+                </div>
+                <div className="pt-2 pb-4 font-bold text-green-700 text-center">
+                  You will save ₹ {discount} on this order
+                </div>
+              </section>
+            </form>
           </div>
-        </div>
-      )}
+        </>
+      )
+      }
     </>
   );
 };
